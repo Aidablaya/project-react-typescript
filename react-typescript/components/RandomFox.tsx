0242ef5c1ebbe578a1447f7aef7f8ebf639f2ded
Aidablaya <you@example.com>
2023-10-10 13:09:40 +0200

    types

---
+++ react-typescript/components/RandomFox.tsx
@@ -0,0 +1,9 @@
+import {useRef} from 'react';
+
+type Props = {image:string};
+
+export const RandomFox = ({image}: Props): JSX.Element => {
+    const node = useRef<HTMLImageElement>(null)
+    
+    return <img ref={node} width={320} height="auto" className="rounded"  src={image} />
+}
--- react-typescript/pages/index.tsx
+++ react-typescript/pages/index.tsx
@@ -1,23 +1,52 @@
-
+import { useState } from "react";
 import type { NextPage } from "next";
-import Head from "next/head"; //7.4k (gzipped: 2.9)
+import type { MouseEventHandler } from "react";
+import Head from "next/head"; 
+import { RandomFox } from "../components/RandomFox";
+
+//generate a random function between 1 and 123
+const random = (): number => Math.floor(Math.random()*123)+1
+
+//generate simple unique id
+const generateId = () => Math.random().toString(36).substr(2, 9)
+
+type ImageItem = { id: string; url: string }; 
 
 const Home: NextPage = () => {
- return (
-  <div>
-    <Head>
-      <title>Platzi</title>
-      <meta name="description" content="Generate by Platzi" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <main>
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </main>
-    <footer></footer>
-  </div>
- )
+  const [images, setImages] = useState <Array<ImageItem>>([]);
+
+const addNewFox:MouseEventHandler<HTMLButtonElement> = (event) => {
+  event.preventDefault();
+
+  const target = event.target;
+
+  const newImageItem: ImageItem = {
+    id: generateId(),
+    url: `https://randomfox.ca/images/${random()}.jpg`,
+  };
+
+    setImages([...images,newImageItem])
+  };
+
+  return (
+    <div>
+      <Head>
+        <title>Platzi</title>
+        <meta name="description" content="Generate by Platzi" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <button onClick={addNewFox}>Add new fox</button>
+        {images.map((image) => (
+          <div key={image.id} className="p-4">
+            <RandomFox image={image.url} />
+          </div>
+        ))}
+      </main>
+      <footer></footer>
+    </div>
+  );
 };
 
 export default Home;
